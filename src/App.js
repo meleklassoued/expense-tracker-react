214b6f942825a3d355b9de59cb733120a867262a
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-12-26 19:17:39 +0100

    add all of the compoenents to the app.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
