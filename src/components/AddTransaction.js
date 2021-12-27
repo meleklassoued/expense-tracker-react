b9e58e1b92b32e54099526a1afe3975074355b9c
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-12-27 01:10:51 +0100

    complete some states in transactions store

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,25 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function AddTransaction() {
-    return (
-      <>
-        <h3>Add new transaction</h3>
-        <form id='form'>
-          <div className='form-control'>
-            <label for='text'>Text</label>
-            <input type='text' id='text' placeholder='Enter text...' />
-          </div>
-          <div className='form-control'>
-            <label for='amount'>
-              Amount <br />
-              (negative - expense, positive - income)
-            </label>
-            <input type='number' id='amount' placeholder='Enter amount...' />
-          </div>
-          <button className='btn'>Add transaction</button>
-        </form>
-      </>
-    );
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form id='form'>
+        <div className='form-control'>
+          <label htmlFor='text'>Text</label>
+          <input
+            type='text'
+            id='text'
+            placeholder='Enter text...'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='amount'>
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type='number'
+            id='amount'
+            placeholder='Enter amount...'
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <button className='btn'>Add transaction</button>
+      </form>
+    </>
+  );
 }
 
-export default AddTransaction
+export default AddTransaction;
