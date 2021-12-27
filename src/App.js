ac9bc7b116e42d91ecc757a9e9cb11337f86e00c
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-12-27 01:11:45 +0100

    drilling up the props with useContext

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Balance from "./components/Balance";
 import IncomeExpenses from "./components/IncomeExpenses";
 import TransactionList from "./components/TransactionList";
 import AddTransaction from "./components/AddTransaction";
+import { GlobalProvider, GlobalContext } from "./context/GlobalState";
 
 function App() {
+  const { transactions } = React.useContext(GlobalContext);
+
   return (
-    <div>
-      <Header />
-      <div className='container'>
+    <div className='container'>
+      <GlobalProvider>
+        <Header />
         <Balance />
         <IncomeExpenses />
-        <TransactionList />
+        <TransactionList Transactions={transactions} />
         <AddTransaction />
-      </div>
+      </GlobalProvider>
     </div>
   );
 }
