18003c2e4d12ee952aea618efcbdd6cd7ede582c
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-01-01 02:35:50 +0100

    addprops for balance

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className='container'>
       <GlobalProvider>
         <Header />
-        <Balance />
+        <Balance Transactions={transactions} />
         <IncomeExpenses />
         <TransactionList Transactions={transactions} />
         <AddTransaction />
