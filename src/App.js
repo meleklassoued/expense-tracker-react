eba3240ecf55f35c9d54475cfb37b699ad60c4b8
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-01-07 18:11:32 +0100

    fix the bugs in this two files

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import Balance from "./components/Balance";
 import IncomeExpenses from "./components/IncomeExpenses";
 import TransactionList from "./components/TransactionList";
 import AddTransaction from "./components/AddTransaction";
-import { GlobalProvider, GlobalContext } from "./context/GlobalState";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
-  const { transactions } = React.useContext(GlobalContext);
-
   return (
     <div className='container'>
       <GlobalProvider>
         <Header />
-        <Balance Transactions={transactions} />
-        <IncomeExpenses />
-        <TransactionList Transactions={transactions} />
-        <AddTransaction />
+        <div className='container'>
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+        </div>
       </GlobalProvider>
     </div>
   );
