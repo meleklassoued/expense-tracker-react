9a9da6e1b0402e55a2eb98936181b06a0f3d0467
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-01-07 16:34:31 +0100

    complete the incomeexpense component

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,18 +1,31 @@
 import React from "react";
 
-function IncomeExpenses() {
+function IncomeExpenses({ Transactions }) {
+  const amounts = Transactions.map((transaction) => transaction.amount);
+  console.log(amounts);
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  console.log(income);
+
+  const outcome = amounts
+    .filter((item) => item < 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
   return (
     <div className='inc-exp-container'>
       <div>
         <h4>Income</h4>
         <p id='money-plus' className='money plus'>
-          +$0.00
+          +${income}
         </p>
       </div>
       <div>
         <h4>Expense</h4>
         <p id='money-minus' className='money minus'>
-          -$0.00
+          -${outcome}
         </p>
       </div>
     </div>
